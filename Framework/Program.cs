2f10b492b5191634c2800b7238af56285cd1d642
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2020-02-11 09:23:01 +0900

    Add tests.

---
+++ Framework/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if false
+            Test.Font.Test(args);
+#endif
+#if false
+            Test.Runtime.Test(args);
+#endif
+#if false
+            Test.Culture.Test(args);
+#endif
+#if true
+            Test.Spans.Test(args);
+#endif
+        }
+    }
+}
